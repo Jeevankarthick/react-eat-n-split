04c9e82c98493cae8c4a0d9efd3d2aaac3e0c50f
Jeevankarthick B <jeevankarthick232@gmail.com>
2023-11-14 21:58:20 +0530

    add key prop to FormSplitBill to reset state - diffing concept

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onBillSplit={handleBillSplit}
+          key={selectedFriend.id}
         />
       )}
     </div>
